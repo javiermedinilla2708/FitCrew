38b8ca16f15e0a055c45894a76c6bf7d598111a3
JavierMedinilla21 <al.javier.medinilla.dominguez@iesportada.org>
2026-03-07 20:02:33 +0100

    Creación de los modelos de datos constantes de la app

---
+++ lib/models/app_constants.dart
@@ -0,0 +1,19 @@
+class AppConstants {
+  static const List<String> availableSports = [
+    "Padel",
+    "Running",
+    "Yoga",
+    "Crossfit",
+    "Ciclismo",
+    "Fútbol",
+    "Basket",
+    "Tenis"
+  ];
+
+  static const List<String> skillLevels = [
+    "Principiante",
+    "Intermedio",
+    "Avanzado",
+    "Profesional"
+  ];
+}
