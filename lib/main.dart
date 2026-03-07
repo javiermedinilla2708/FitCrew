e8e79b0050afc1fe66d57ce11312a19fb380f84a
JavierMedinilla21 <al.javier.medinilla.dominguez@iesportada.org>
2026-03-07 21:18:07 +0100

    Importacion de firebase al proyecto

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
-import 'package:fitcrew/viewmodels/activity_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+import 'package:fitcrew/viewmodels/activity_view_model.dart';
 import 'package:fitcrew/screens/login/login_screen.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(
       providers: [
