7fa777324de7de762d9884fa3045ecf5dfb778f9
JavierMedinilla21 <al.javier.medinilla.dominguez@iesportada.org>
2026-03-08 14:06:01 +0100

    Logica de autorización de logeo con FireBase

---
+++ lib/services/auth_services.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService{
+  final FirebaseAuth _auth=FirebaseAuth.instance;
+
+  //Registro con email y contraseña
+  Future<User?>registerWithEmail(String email,String password)async{
+    try{
+      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      return result.user;
+    }catch(e){
+      print("Error en registro:${e.toString()}");
+      return null;
+    }
+  }
+
+  //Login con email y contraseña
+  Future <User?> loginWithEmail(String email,String password)async{
+    try{
+      UserCredential result=await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return result.user;
+    }catch(e){
+      print("Error en login: ${e.toString()}");
+      return null;
+    }
+  }
+
+  //Cerrar sesion
+  Future <void> signOut()async{
+    await _auth.signOut();
+  }
+}
